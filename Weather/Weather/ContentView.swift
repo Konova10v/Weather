c34b2e40d234bbebac58d8fe11c6547cdd7060fd
Konova10v <https://github.com/Konova10v>
2020-08-21 23:45:36 +0500

    Начало проекта

---
+++ Weather/Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Кирилл Коновалов on 21.08.2020.
+//  Copyright © 2020 Кирилл Коновалов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
